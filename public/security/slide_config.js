45ebc0e0af3f0f6e594d407ff0d1fd28f1670e90
Lars-m <larsesspam@gmail.com>
2022-03-10 15:42:17 +0100

    Update slide_config.js

--- public/security/slide_config.js
+++ public/security/slide_config.js
@@ -3,7 +3,7 @@ var SLIDE_CONFIG = {
   settings: {
     title: 'Security',
     subtitle: 'Authentication in SinglePage/App-Applications and handling passwords',
-    date: 'Spring - 2018',
+    date: 'Spring - 2022',
     //eventInfo: {
     //  title: 'Google I/O',
     //  date: '6/x/2013'
